66d76064296db2665ee67cc4971f04d8134caf9d
Dinesh <53552105+rdinesh808@users.noreply.github.com>
2025-03-13 20:03:22 +0530

    Added New File

---
+++ python_slicing.py
@@ -0,0 +1,21 @@
+py_list = [1,2,3,4,5,6,7,8,9,10]
+
+print(py_list)
+
+print(py_list[::-1]) # Reverse list
+
+print(py_list[::]) # Same as py_list
+
+print(py_list[::2]) # Odd Numbers
+
+print(py_list[1::2]) # Even Numbers
+
+print(py_list[1:6:]) # Print index 1 to 5
+
+print(py_list[-5:-1]) # start from index -5 and end -2
+
+print(py_list[1:-5])
+
+print(py_list[3:-1])
+
+print(py_list[-5::])
